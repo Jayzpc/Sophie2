6f72bd169610c38018294719758b5c95cd5b8b64
jayzpc <jb6197654@gmail.com>
2026-02-01 14:49:56 -0500

    backup commit Feb 1

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -12,95 +12,99 @@
 
 // use try / catch to handle errors
 
-async function displayGallery(category) {
+async function displayGallery(category, selector = '.gallery') {
 
-//where the data is going
-const portfolioElement = document.getElementById('portfolio')
-
-
-try {
-    
-    const response = await fetch('http://localhost:5678/api/works')
-    if (response.ok) {
-     const data = await response.json()
-                //This will look for first element in portfolio with name gallery
-    const galleryElement = portfolioElement.querySelector('.gallery')
-               // console.log(data) or displayData(data);
-
-               
-                
-
-                // Loop through each item in the data array
-let result = ''
-// Build the HTML structure for each item speicifying what to show
-// the word item is the type of data from swagger documentation
-data.forEach(item => {
-
-    //if we want a particular item ex item.category.Id === 1 add this line
-    // you 
-    //if (item.categoryId === 1) { 
-// || is logical OR
-    if (item.categoryId === category || category === undefined) {
+    //where the data is going
+   // const portfolioElement = document.getElementById('portfolio')
 
 
-result += `<figure>
+    try {
+
+        const response = await fetch('http://localhost:5678/api/works')
+        if (response.ok) {
+            const data = await response.json()
+            //This will look for first element in portfolio with name gallery
+            const galleryElement = document.querySelector(selector)
+            // console.log(data) or displayData(data);
+
+
+
+
+            // Loop through each item in the data array
+            let result = ''
+            // Build the HTML structure for each item speicifying what to show
+            // the word item is the type of data from swagger documentation
+            data.forEach(item => {
+
+                //if we want a particular item ex item.category.Id === 1 add this line
+                // you 
+                //if (item.categoryId === 1) { 
+                // || is logical OR
+                if (item.categoryId === category || !category) {
+
+
+                    result += `<figure>
             <img src="${item.imageUrl}" alt="${item.title}">
             <figcaption>"${item.title}"</figcaption>
             </figure>`
+                }
+            })
+
+
+            galleryElement.innerHTML = result;
+
+        }
+
+    } catch (error) {
+
+        console.error(error)
     }
-})
-    
-
-galleryElement.innerHTML = result;
-
-}
-
-           } catch (error) {
-
-   console.error(error)
-}
 }
 
 // Call the function to display the gallery when the page loads
 // if you want to filter by category add a number in the brackets
-// displayGallery()
+ displayGallery(null, "#portfolio-gallery")
 
 const galleryModal = document.getElementById('galleryModal')
+const galleryModalBtn = document.getElementById("gallery-popover")
 const galleryFormButton = document.getElementById('gallery-form-button')
 
+galleryModalBtn.addEventListener('click', () => {
+   displayGallery (null, ".galleryDelete .gallery")
+})
 
 galleryFormButton.addEventListener('click', () => {
-const galleryDeleteElement = galleryModal.querySelector('.galleryDelete')
-const galleryFormElement = galleryModal.querySelector('.galleryForm')
+    const galleryDeleteElement = galleryModal.querySelector('.galleryDelete')
+    const galleryFormElement = galleryModal.querySelector('.galleryForm')
 
-galleryDeleteElement.classList.remove('show')
-galleryFormElement.classList.add('show')
+    galleryDeleteElement.classList.remove('show')
+    galleryFormElement.classList.add('show')
 
+    /*
+          const modalpop = document.querySelector('galleryModal.galleryDelete');
+            // Clear the gallery before displaying new or filtered works
+            modalpop.innerHTML = 'result';
+    const figure = document.createElement('figure');
+    const trash = document.createElement('button');
+    trash.addEventListener('click', async (event) => {
+                trash.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    
+                /*trash.textContent = "trash"; //  
+                trash.classList.add('trash');
+                figure.classList.add('positioned-element');
+                const modalimg = document.createElement('img');
+                /*const figcaption = document.createElement('figcaption');
+    
+                figure.appendChild(trash);
+    
+                modalimg.src = item.imageUrl;
+                modalimg.classList.add('modalimage');
+                            figure.appendChild(modalimg);
+                /*figure.appendChild(figcaption);
+                modalpop.appendChild(figure);
+    modalpop.appendChild(item.categoryId);*/
 
-      const modalpop = document.querySelector('galleryModal.galleryDelete');
-        // Clear the gallery before displaying new or filtered works
-        modalpop.innerHTML = 'result';
-const figure = document.createElement('figure');
-const trash = document.createElement('button');
-trash.addEventListener('click', async (event) => {
-            trash.innerHTML = '<i class="fa-solid fa-trash"></i>';
-
-            /*trash.textContent = "trash"; //  */
-            trash.classList.add('trash');
-            figure.classList.add('positioned-element');
-            const modalimg = document.createElement('img');
-            /*const figcaption = document.createElement('figcaption');*/
-
-            figure.appendChild(trash);
-
-            modalimg.src = item.imageUrl;
-            modalimg.classList.add('modalimage');
-                        figure.appendChild(modalimg);
-            /*figure.appendChild(figcaption);*/
-            modalpop.appendChild(figure);
-modalpop.appendChild(item.categoryId);
-
-})})
+})
 
 
 
